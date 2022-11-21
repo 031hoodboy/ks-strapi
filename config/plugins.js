1707f65edcb28a9150a1155affd10c930b148804
031hoodboy <littlepark0107@gmail.com>
2022-11-21 16:52:00 +0900

    updeat env

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_0XEjelbxc9XT4li3BgUcAplysqqq/Zz2hQfv9AA",
-      apiToken: "3NlVYoOjMTWIU1ckXuZfFLTl",
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
