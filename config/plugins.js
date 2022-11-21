cfd8a5cfb4ff0a4ae1d45a7ad18cfe14c3f9f802
031hoodboy <littlepark0107@gmail.com>
2022-11-21 17:12:38 +0900

    node version to 14

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,6 @@ module.exports = ({ env }) => ({
     config: {
       deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
       apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
