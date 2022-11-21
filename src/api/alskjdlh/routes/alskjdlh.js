713cbaa6fc4582400aacb94dd08782dd0e75e34c
031hoodboy <littlepark0107@gmail.com>
2022-11-21 18:41:27 +0900

    fix err

---
+++ src/api/alskjdlh/routes/alskjdlh.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * alskjdlh router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::alskjdlh.alskjdlh');
---
+++ src/api/alskjdlh/services/alskjdlh.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * alskjdlh service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::alskjdlh.alskjdlh');
